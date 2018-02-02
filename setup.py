f0ea401990c7111479274f9b00a1b550107a652a
pub@matthias-henning.de <pub@matthias-henning.de>
2018-02-02 20:14:01 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='ciliatus_controlunit',
-    version='v0.2-beta',
-    packages=['', 'system', 'system.sensors', 'worker'],
-    url='https://github.com/matthenning/ciliatus_controlunit',
+    version='v1.0-RC1',
+    packages=['', 'system', 'system.sensors', 'system.components' 'worker'],
+    url='https://github.com/ciliatus/ciliatus-controlunit-python',
     license='GPL-3.0',
     author='Matthias Henning',
     author_email='info@ciliatus.io',
-    description='Controlunit for ciliatus'
+    description='Controlunit for Ciliatus'
 )
