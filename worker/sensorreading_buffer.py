0834eb059282ca2084d3755372b9fc115c1dd3ea
pub@matthias-henning.de <pub@matthias-henning.de>
2018-02-10 22:49:15 +0100

    typo in log text

--- worker/sensorreading_buffer.py
+++ worker/sensorreading_buffer.py
@@ -61,7 +61,7 @@ class SensorreadingBuffer(Process):
             if result is None:
                 self.stash.prepend(item)
                 self.logger.warning(
-                    'SensorreadingBuffer.run(): Push failed with not error. Item was returned to stash (size: %i) for '
+                    'SensorreadingBuffer.run(): Push failed with no error. Item was returned to stash (size: %i) for '
                     'PS %s LS %s',
                     self.stash.count(), item['sensor'].name, str(item['payload']['id'])
                 )
