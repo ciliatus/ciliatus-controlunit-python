acf126a0452bdeab7c65d87d3cc8821f823be992
pub@matthias-henning.de <pub@matthias-henning.de>
2019-07-19 23:51:48 +0200

    added performance counters to maintenance thread

--- worker/maintenance.py
+++ worker/maintenance.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import datetime
+import time
 import urllib
+import os
+import psutil
 from multiprocessing import Process
 
 import system.log as log
@@ -27,13 +30,20 @@ class Maintenance(Process):
         pass
 
     def __check_in(self):
+        process = psutil.Process(os.getpid())
         with api_client.ApiClient('controlunits/%s/check_in' % self.id) as api:
             result = None
             try:
                 client_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 result = api.call({
                     'software_version': self.version,
-                    'client_time': client_time
+                    'client_time': client_time,
+                    'cmd_line': " ".join(list(process.cmdline())),
+                    'cpu_time': process.cpu_times().user,
+                    'memory_usage': process.memory_info().rss,
+                    'io_read_bytes': process.io_counters().read_bytes,
+                    'io_write_bytes': process.io_counters().write_bytes,
+                    'uptime_seconds': time.time() - process.create_time()
                 }, 'PUT')
                 self.logger.debug('Maintenance.__check_in(): Pushed client time %s' % client_time)
             except urllib.error.HTTPError as err:
